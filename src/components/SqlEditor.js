c4b0df0c35f0476b49127e0c7f218c744bddc10a
Vedansh Shrivastava <vedanshshrivastava3@gmail.com>
2025-03-30 01:42:22 +0530

    editor

--- src/components/SqlEditor.js
+++ src/components/SqlEditor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-sql";
 import "ace-builds/src-noconflict/theme-monokai";
@@ -10,23 +10,45 @@ import "ace-builds/src-noconflict/theme-nord_dark";
 import "ace-builds/src-noconflict/ext-language_tools";
 import { Dropdown } from "react-bootstrap";
 
-const SqlEditor = ({ queryText, setQueryText }) => { 
-  const [theme, setTheme] = useState('solarized_light');
+const SqlEditor = ({ queryText, setQueryText, onRunQuery }) => { 
+  const [theme, setTheme] = useState("solarized_light");
 
   const handleTheme = (themeName) => {
     setTheme(themeName);
-  }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.ctrlKey && event.key === "Enter") {
+        event.preventDefault(); 
+        onRunQuery(); 
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [onRunQuery]); 
 
   return (
     <div className="sql-editor-container">
       <Dropdown className="theme-selector">
         <Dropdown.Toggle>Theme</Dropdown.Toggle>
         <Dropdown.Menu>
-          <Dropdown.Item onClick={() => handleTheme('monokai')} active={theme==='monokai'}>Monokai</Dropdown.Item>
-          <Dropdown.Item onClick={() => handleTheme('solarized_dark')} active={theme==='solarized_dark'}>Solarized Dark</Dropdown.Item>
-          <Dropdown.Item onClick={() => handleTheme('solarized_light')} active={theme==='solarized_light'}>Solarized Light</Dropdown.Item>
-          <Dropdown.Item onClick={() => handleTheme('tomorrow_night')} className={`${theme === 'tomorrow_night' ? 'theme-active' : ''}`}>Tomorrow Night</Dropdown.Item>
-          <Dropdown.Item onClick={() => handleTheme('nord_dark')} className={`${theme === 'nord_dark' ? 'theme-active' : ''}`}>Nord Dark</Dropdown.Item>
+          <Dropdown.Item onClick={() => handleTheme("monokai")} active={theme === "monokai"}>
+            Monokai
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleTheme("solarized_dark")} active={theme === "solarized_dark"}>
+            Solarized Dark
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleTheme("solarized_light")} active={theme === "solarized_light"}>
+            Solarized Light
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleTheme("tomorrow_night")} active={theme === "tomorrow_night"}>
+            Tomorrow Night
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => handleTheme("nord_dark")} active={theme === "nord_dark"}>
+            Nord Dark
+          </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
       <AceEditor
